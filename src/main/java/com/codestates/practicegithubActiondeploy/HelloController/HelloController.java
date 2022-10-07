e6ef691139eb2e9052589b9818fe62f12a906fce
pjnw1236 <pjnw1236@gmail.com>
2022-10-07 15:38:31 +0900

    edit HelloController

--- src/main/java/com/codestates/practicegithubActiondeploy/HelloController/HelloController.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/HelloController/HelloController.java
@@ -1,9 +1,9 @@
 package com.codestates.practicegithubActiondeploy.HelloController;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
     @GetMapping("/")
     public String hello() {
